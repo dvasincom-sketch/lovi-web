3ae4625597fd0b8704e14910c7188fceacb60f8f
Dmitry Vasin <dmitryvasin@MacBook-Pro-Dmitry.local>
2026-05-07 20:07:05 +0300

    fix: SW navigate fallback to index.html, exclude API routes

--- vite.config.js
+++ vite.config.js
@@ -27,7 +27,8 @@ export default defineConfig({
             handler: 'NetworkOnly'
           }
         ],
-        navigateFallback: '/offline.html'
+        navigateFallback: '/index.html',
+        navigateFallbackDenylist: [/^\/api/, /\.\w+$/]
       },
       manifest: {
         name: 'Lovi — горящие окошки',
